25b82e9eaf0e67807ebd99849cf11ca704e4215a
TiagoDomin-os <tiagodomino@hotmail.com>
2024-01-25 23:25:30 +0000

    Primeiro commit

---
+++ src/firebase/config.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAE0qsk7ox3PcI0pumZVhV5f5QOirecoh4",
+    authDomain: "portefolio-valadares.firebaseapp.com",
+    projectId: "portefolio-valadares",
+    storageBucket: "portefolio-valadares.appspot.com",
+    messagingSenderId: "902190186784",
+    appId: "1:902190186784:web:40bb4a5bc8d84aab369745"
+  };
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore};
