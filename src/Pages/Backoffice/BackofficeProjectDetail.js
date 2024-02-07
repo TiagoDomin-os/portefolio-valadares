95a0a3323dffccb53116a0bbf0d45483092b1b37
TiagoDomin-os <tiagodomino@hotmail.com>
2024-02-07 15:38:18 +0000

    ""

--- src/Pages/Backoffice/BackofficeProjectDetail.js
+++ src/Pages/Backoffice/BackofficeProjectDetail.js
@@ -12,7 +12,7 @@ const BackofficeProjectDetails = () => {
   const { projectId } = useParams();
   const navigate = useNavigate();
   const [nome, setNome] = useState('');
-  const [autores, setAutores] = useState(['']); // autores deve ser um array para suportar múltiplos autores
+  const [autores, setAutores] = useState(['']);
   const [categoria, setCategoria] = useState('');
   const [newFeaturedImage, setNewFeaturedImage] = useState(null);
   const [youtubeLinks, setYoutubeLinks] = useState([]);
@@ -31,7 +31,7 @@ const BackofficeProjectDetails = () => {
         const data = docSnap.data();
         setExistingData(data);
         setNome(data.nome);
-        setAutores(data.autores || ['']); // Definindo autores como array
+        setAutores(data.autores || ['']);
         setCategoria(data.categoria);
         setYoutubeLinks(data.youtubeLinks || []);
         setGaleria(data.galeria || []);
@@ -45,13 +45,12 @@ const BackofficeProjectDetails = () => {
     fetchProject();
   }, [projectId, navigate]);
 
-
-  
-  const handleAutorChange = (index, value) => {
+  const handleAutorChange = (index, newValue) => {
     const updatedAutores = [...autores];
-    updatedAutores[index] = value;
+    updatedAutores[index] = newValue;
     setAutores(updatedAutores);
   };
+  
 
   const addAutorInput = () => {
     setAutores([...autores, '']);
@@ -61,28 +60,81 @@ const BackofficeProjectDetails = () => {
     const filteredAutores = autores.filter((_, i) => i !== index);
     setAutores(filteredAutores);
   };
+  
+
+  const handleImageChange = (e) => {
+    if (e.target.files[0]) {
+      setNewFeaturedImage(e.target.files[0]);
+    }
+  };
+
+  const handleGalleryChange = (e) => {
+    const newFiles = Array.from(e.target.files);
+    setGaleria([...galeria, ...newFiles]);
+  };
+
+  const handleYoutubeLinkChange = (index, value) => {
+    const updatedLinks = [...youtubeLinks];
+    updatedLinks[index] = value;
+    setYoutubeLinks(updatedLinks);
+  };
+
+  const addYoutubeLinkInput = () => {
+    setYoutubeLinks([...youtubeLinks, '']);
+  };
+
+  const removeYoutubeLinkInput = (index) => {
+    const updatedLinks = youtubeLinks.filter((_, idx) => idx !== index);
+    setYoutubeLinks(updatedLinks);
+  };
+
+  const removeGalleryItem = async (index) => {
+    const itemToRemove = galeria[index];
+    if (typeof itemToRemove === "string") {
+      const fileRef = ref(projectStorage, itemToRemove);
+      try {
+        await deleteObject(fileRef);
+        const updatedGaleria = existingData.galeria.filter((_, idx) => idx !== index);
+        await updateDoc(doc(projectFirestore, 'projetos', projectId), { galeria: updatedGaleria });
+      } catch (error) {
+        console.error("Erro ao remover arquivo do Storage:", error);
+      }
+    } else {
+      const updatedGaleria = galeria.filter((_, idx) => idx !== index);
+      setGaleria(updatedGaleria);
+    }
+  };
 
   const handleUpdateProject = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
     try {
-      const imageUrl = newFeaturedImage ? await fileUploader.uploadFile(newFeaturedImage, 'featuredImages', setUploadProgress) : existingData.featuredImage;
-     
-      const newGalleryUrls = await Promise.all(galeria.map(file => 
-        file instanceof File ? fileUploader.uploadFile(file, 'gallery', setUploadProgress) : Promise.resolve(file)
-      ));
-
+      let imageUrl = existingData.featuredImage;
+      if (newFeaturedImage) {
+        const uploadResult = await fileUploader.uploadFile(newFeaturedImage, 'featuredImages', setUploadProgress);
+        if (uploadResult) {
+          imageUrl = uploadResult;
+        } else {
+          // Se o upload falhar, mantém a imagem existente ou define uma fallback
+          imageUrl = existingData.featuredImage || 'URL_DA_IMAGEM_PADRAO';
+        }
+      }
 
       const autoresList = autores.filter(autor => autor.trim() !== '');
 
+
+      const newGalleryUrls = await Promise.all(galeria.map(file =>
+        !(file instanceof File) ? file : fileUploader.uploadFile(file, 'gallery', (progress) => setUploadProgress(progress))
+      ));
+
       const updatedProject = {
         nome,
-        autores: autoresList, // Atualizar para usar a lista de autores
+        autores,
         categoria,
         featuredImage: imageUrl,
         youtubeLinks,
-        galeria: newGalleryUrls
+        galeria: newGalleryUrls.filter(url => url !== undefined) // Filtra URLs indefinidas
       };
 
       await updateDoc(doc(projectFirestore, 'projetos', projectId), updatedProject);
@@ -96,99 +148,39 @@ const BackofficeProjectDetails = () => {
     }
   };
 
-  const handleImageChange = (e) => {
-    if (e.target.files[0]) {
-      setNewFeaturedImage(e.target.files[0]);
-    }
-  };
-
-  const handleGalleryChange = (e) => {
-    const newFiles = Array.from(e.target.files);
-    setGaleria([...galeria, ...newFiles]);
-  };
-
-
-
-  const addYoutubeLinkInput = () => {    setYoutubeLinks([...youtubeLinks, '']);  };
-
-  const handleYoutubeLinkChange = (index, value) => {
-    const updatedLinks = [...youtubeLinks];
-    updatedLinks[index] = value;
-    setYoutubeLinks(updatedLinks);
-  };
-
-  const removeYoutubeLinkInput = (index) => {
-    const updatedLinks = youtubeLinks.filter((_, idx) => idx !== index);
-    setYoutubeLinks(updatedLinks);
-  };
-
-  
-
-  const removeGalleryItem = async (index) => {
-    const itemToRemove = galeria[index];
-  
-    // Verifica se o item é um arquivo (não enviado ainda) ou um URL (já enviado)
-    if (typeof itemToRemove === "string") {
-      // Construa a referência ao arquivo no Firebase Storage
-      const fileRef = ref(projectStorage, itemToRemove);
-  
-      try {
-        // Tenta deletar o arquivo do Storage
-        await deleteObject(fileRef);
-        console.log("Arquivo removido do Storage:", itemToRemove);
-  
-        // Remove o item do array no Firestore também
-        const updatedGaleria = existingData.galeria.filter((_, idx) => idx !== index);
-        await updateDoc(doc(projectFirestore, 'projetos', projectId), { galeria: updatedGaleria });
-      } catch (error) {
-        console.error("Erro ao remover arquivo do Storage:", error);
-      }
-    } else {
-      // É um arquivo local, apenas remove do estado local
-      const updatedGaleria = galeria.filter((_, idx) => idx !== index);
-      setGaleria(updatedGaleria);
-    }
-  };
-
-
-
-
-
-
   return (
     <>
-    
       <BackofficeNavbar />
       <div className="container mt-5">
         <h2>Editar Projeto</h2>
-        {isSubmitting ? (
-          <p>Atualizando projeto...</p>
-        ) : (
+        {isSubmitting ? <p>Atualizando projeto...</p> : (
           <form onSubmit={handleUpdateProject} className="mb-3">
-            {/* Nome do Projeto */}
-            <div className="mb-3">
+ {/* Nome do Projeto */}
+ <div className="mb-3">
               <label htmlFor="nome" className="form-label">Nome do Projeto</label>
               <input type="text" className="form-control" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} />
             </div>
 
             {/* Autores */}
+            <label htmlFor="nome" className="form-label">Autores</label>
+
             {autores.map((autor, index) => (
-              <div key={index} className="mb-3 d-flex">
-                <input
-                  type="text"
-                  className="form-control"
-                  value={autor}
-                  onChange={(e) => handleAutorChange(e, index)}
-                  placeholder="Nome do Autor"
-                />
-                <button type="button" className="btn btn-danger ml-2" onClick={() => removeAutorInput(index)}>
-                  Remover
-                </button>
-              </div>
-            ))}
-            <button type="button" className="btn btn-secondary mb-3" onClick={addAutorInput}>
-              Adicionar Autor
-            </button>
+  <div key={index} className="mb-3 d-flex">
+    <input
+      type="text"
+      className="form-control"
+      value={autor}
+      onChange={(e) => handleAutorChange(index, e.target.value)}
+      placeholder="Nome do Autor"
+    />
+    <button type="button" className="btn btn-danger ml-2" onClick={() => removeAutorInput(index)}>
+      Remover
+    </button>
+  </div>
+))}
+<button type="button" className="btn btn-secondary" onClick={addAutorInput}>Adicionar Autor</button>
+
+
 
             {/* Categoria */}
             <div className="mb-3">
@@ -236,20 +228,27 @@ const BackofficeProjectDetails = () => {
             </div>
 
             <div className="mb-3">
-            <label htmlFor="galeria" className="form-label">Galeria de Imagens</label>
-            <input type="file" className="form-control" id="galeria" multiple onChange={handleGalleryChange} />
-            <div className="mt-2">
-              {galeria.map((image, index) => (
-                <div key={index} className="d-flex align-items-center mb-2">
-                  {/* Exibição das imagens */}
-                  {/* ... */}
-                  <button type="button" className="btn btn-danger ml-2" onClick={() => removeGalleryItem(index)}>
-                    Remover
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
+  <label htmlFor="galeria" className="form-label">Galeria de Imagens</label>
+  <input type="file" className="form-control" id="galeria" multiple onChange={handleGalleryChange} />
+  <div className="mt-2">
+    {existingData.galeria && existingData.galeria.map((url, index) => (
+      <div key={index} className="d-flex align-items-center mb-2">
+        <img src={url} alt={`Imagem ${index}`} style={{ maxWidth: '100px', maxHeight: '100px' }} />
+        <button type="button" className="btn btn-danger ml-2" onClick={() => removeGalleryItem(url, 'existing')}>
+          Remover
+        </button>
+      </div>
+    ))}
+    {galeria.filter(file => file instanceof File).map((file, index) => (
+      <div key={`new-${index}`} className="d-flex align-items-center mb-2">
+        <img src={URL.createObjectURL(file)} alt={`Nova imagem ${index}`} style={{ maxWidth: '100px', maxHeight: '100px' }} onLoad={() => URL.revokeObjectURL(file)} />
+        <button type="button" className="btn btn-danger ml-2" onClick={() => removeGalleryItem(index, 'new')}>
+          Remover
+        </button>
+      </div>
+    ))}
+  </div>
+  </div>
 
           {/* Indicador de Progresso de Upload, colocado fora do loop da galeria */}
           {isSubmitting && (
@@ -263,12 +262,11 @@ const BackofficeProjectDetails = () => {
           {/* Botão para salvar as alterações */}
           <button type="submit" className="btn btn-primary" disabled={isSubmitting}>
             Salvar Alterações
-          </button>
-        </form>
-      )}
-    </div>
-  </>
-);
-          }
+          </button>          </form>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default BackofficeProjectDetails;
